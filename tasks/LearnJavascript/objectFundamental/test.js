376e6b569d4ff823440f85c3908a3d218c911a65
Гавриш Сергій <s.havrysh.2107@in.np.gov.ua>
2019-12-03 18:51:15 +0200

    objectFundamental   Calculator()   Accumulator()

--- tasks/LearnJavascript/objectFundamental/test.js
+++ tasks/LearnJavascript/objectFundamental/test.js
@@ -1,5 +1,4 @@
-// import { assert } from 'chai';
-
+/* eslint-disable no-undef */
 describe('isEmpty', () => {
   it('возвращает true для пустого объекта', () => {
     assert.isTrue(isEmpty({}));
@@ -52,5 +51,101 @@ describe('калькулятор', () => {
     it('Произведение равно 6', () => {
       assert.equal(calculator.mul(), 6);
     });
+  });
+});
+
+describe('Лестница', () => {
+  before(() => {
+    window.alert = sinon.stub(window, 'alert');
+  });
+
+  beforeEach(() => {
+    ladder.step = 0;
+  });
+
+  it('up() должен возвращать this', () => {
+    assert.equal(ladder.up(), ladder);
+  });
+
+  it('down() должен возвращать  this', () => {
+    assert.equal(ladder.down(), ladder);
+  });
+
+  it('showStep() должен вызывать alert', () => {
+    ladder.showStep();
+    assert(alert.called);
+  });
+
+  it('up() должен увеличивать ступеньки', () => {
+    assert.equal(ladder.up().up().step, 2);
+  });
+
+  it('down() должен уменьшать ступеньки', () => {
+    assert.equal(ladder.down().step, -1);
+  });
+
+  it('down().up().up().up() ', () => {
+    assert.equal(ladder.down().up().up().up().step, 2);
+  });
+
+  after(() => {
+    ladder.step = 0;
+    alert.restore();
+  });
+});
+
+describe('calculator', () => {
+  let calculator;
+  before(() => {
+    sinon.stub(window, 'prompt');
+
+    prompt.onCall(0).returns('2');
+    prompt.onCall(1).returns('3');
+
+    calculator = new Calculator();
+    calculator.read();
+  });
+
+  it('when 2 and 3 are entered, the sum is 5', () => {
+    assert.equal(calculator.sum(), 5);
+  });
+
+  it('when 2 and 3 are entered, the product is 6', () => {
+    assert.equal(calculator.mul(), 6);
+  });
+
+  after(() => {
+    prompt.restore();
+  });
+});
+
+describe('Accumulator', () => {
+
+  beforeEach(() => {
+    sinon.stub(window, 'prompt');
+  });
+
+  afterEach(() => {
+    prompt.restore();
+  });
+
+  it('initial value is the argument of the constructor', () => {
+    const accumulator = new Accumulator(1);
+
+    assert.equal(accumulator.value, 1);
+  });
+
+  it('after reading 0, the value is 1', () => {
+    const accumulator = new Accumulator(1);
+    prompt.returns('0');
+    accumulator.read();
+    assert.equal(accumulator.value, 1);
+  });
+
+  it('after reading 1, the value is 2', () => {
+    const accumulator = new Accumulator(1);
+    prompt.returns('1');
+    accumulator.read();
+    assert.equal(accumulator.value, 2);
   });
 });
